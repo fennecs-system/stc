33e8e4579c6c620f3c96f5ba8d912ca5ca6a205f
Fennecs <meswaddle@gmail.com>
2026-02-03 14:32:46 +1100

    chore: credo, deps

--- mix.exs
+++ mix.exs
@@ -21,8 +21,15 @@ defmodule Stc.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ecto, "~> 3.13"},
+      {:highlander, "~> 0.2"},
+      {:horde, "~> 0.8"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+
+      # dev
+      {:credo, "~> 1.7.7", only: [:dev, :test, :sandbox], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test, :sandbox], runtime: false},
     ]
   end
 end
